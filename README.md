bdcd787ffe01a232fa8c7b975b88b06acc05186a
vrich88 <mvrichards@icloud.com>
2023-03-10 02:54:18 -0600

    css styling updates matching the rest of the site; additional js functionallity clean up and progression for local storage; general spelling corrections/adjustments; initialize readme

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # MVR-bootcamp-challenge-4
-Bootcamp Challenge 4:  Coding Quiz Game
+
+## Coding Quiz Game
+This repository is used to help quiz some JavaScript related terms and practices while demonstrating the use of JavaScript to make the website interactive and retain data on local storage of the player to show their scores over time.
+
+## Usage
+Upon loading up this repository in a web browser, the user will be presented with a title screen that shows teh rules and the greeting encouraging them to "start" the quiz. Upon taking teh quiz, the user/player will be run through a series of questions that test their knowledge with a timer countdown serving as their score to encourage quickness in testing.  Wrong answers will penalize the player's time but correct answers will boost it. Once teh quiz is completed or the timer reaches zero, the player will be presented with their scores and prompted to enter their initials to save it to the "Hall of Records" which can also be viewed from the title screen to show the history of the scores for players taking the quiz from the local storage.
+
+[Deployed application]()
+
+![screenshot]()
+![screenshot]()
+![screenshot]()
+![screenshot]()
+
+## Contributors
+Consulting and guidance used in the creation of this repository include peers and resources (in no order) as follows:
+    •Daniel Cohen
+    •Matt Montiel
+    •Justin Marchi
+    •Lindsey Issac
+    •Brian Hamlin
+
+## License
+MIT License
+
+Copyright (c) 2023 vrich88
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,11 @@ let quiz = [
         correct: "B.) Make the website interactive",
     }, { /* Question 2 index 1 */
         query: "2.) What is the element within HTML that contains the JavaScript source?",
-        ansA: "A.) "+"< "+"JSON"+" >",
-        ansB: "B.) "+"< "+"js"+" >",
-        ansC: "C.) "+"< "+"link"+" >",
-        ansD: "D.) "+"< "+"script"+" >", /* correct */
-        correct: "D.) "+"< "+"script"+" >",
+        ansA: "A.) &#60;JSON&#62;",
+        ansB: "B.) &#60;js&#62;",
+        ansC: "C.) &#60;link&#62;",
+        ansD: "D.) &#60;script&#62;", /* correct */
+        correct: "D.) &#60;script&#62;"/* correct is not registering due to special characters at this time is always a wrong answer */
     }, { /* Question 3 index 2 */
         query: "3.) How do you find a HTML element with JavaScript?",
         ansA: "A.) var",
@@ -45,7 +45,7 @@ let quiz = [
     }, { /* Question 7 index 6 */
         query: "7.) How would you put a comment within a line of functional JavaScript code?", 
         ansA: "A.) //",
-        ansB: "B.) <--! -->",
+        ansB: "B.) &#60--! --&#62;",
         ansC: "C.) ' '",
         ansD: "D.) /* */", /* correct */
         correct: "D.) /* */", /* correct */
@@ -89,10 +89,19 @@ let quiz = [
     let ansBBtn = document.querySelector("#ansB");
     let ansCBtn = document.querySelector("#ansC");
     let ansDBtn = document.querySelector("#ansD");
-    // score board section identifier
-    let scoreBoard = document.querySelector("#recordedScores")
-    // high scores button identifier
-    let highScore = document.querySelector("#highScores");
+    // recorded scores area section identifier
+    let grading = document.querySelector("#gradeArea");
+    // instructions for player initials entry w/ grade = timer/score identifier
+    let gradeMessage = document.querySelector("#grade");
+    // initials input identifier
+    let pInitials = document.querySelector("#initialsInput");
+    // save grade button identifier
+    let svGrdBtn = document.querySelector("#saveGradeBtn");
+    // Hall of Records identifiers
+    let rankings = document.querySelector("#recordedScores");
+    let sortedRanks = document.querySelector("#sortedpRanks");
+    let sortedInitials = document.querySelector("#sortedpInitials");
+    let sortedScores = document.querySelector("#sortedpScores");
 // global variables in JavaScript
     let secondsLeft = 100;
     let ansBonus = 0;
@@ -104,8 +113,10 @@ let quiz = [
     let askC = quiz[current].ansC; /* console.log(askC); */
     let askD = quiz[current].ansD; /* console.log(askD); */
     let corrAns = quiz[current].correct; /* console.log(corrAns); */
+    let recordedGrades = JSON.parse(localStorage.getItem("recordedGrades")) || []; console.log(recordedGrades);
 
-// function to set the timer and adjust it to count down by 1 second and display it on the HTML and stop it at 0
+// functions used in quiz functionality
+    // function to set the timer and adjust it to count down by 1 second and display it on the HTML and stop it at 0
     function setTime() {
         let timerInterval = setInterval(function () {
             secondsLeft--;
@@ -113,11 +124,19 @@ let quiz = [
             // checks for a value of 0 in the timer and stops the function from running and call for function to create a append message of 0 in the timer
             if (secondsLeft === 0) {
                 clearInterval(timerInterval);
+                grade();
             };
             // countdown by 1 second calculated in milliseconds
         }, 1000);
     };
-// function to generate quiz question space after start button is clicked
+    // function to stop clock 
+    function stopClock(){
+        if (secondsLeft <= 0) {
+            secondsLeft = 0;
+            grade();
+        }
+    };
+    // function to generate quiz question space after start button is clicked
     function showQuiz() {
         showSection();
             questionEl.innerHTML = askQ;
@@ -126,58 +145,96 @@ let quiz = [
             ansCBtn.innerHTML = askC;
             ansDBtn.innerHTML = askD;
         }; 
-// adds check for correct or wrong answer on user click to add a time "bonus" of +5 or -10 when an answer button is clicked
-    answer.addEventListener("click",checkAns);
-        function checkAns(event) {
-            if (event.target.textContent == corrAns) {
-                alert("Correct! +5 seconds");
-                scoreBonus(5);
-                nextQ();
-            } else {
-                alert("Wrong! -10 seconds");
-                scoreBonus(-10);
-                nextQ();
-            };
+    // adds check for correct or wrong answer on user click to add a time "bonus" of +5 or -10 when an answer button is clicked
+    function checkAns(event) {
+        if (event.target.textContent == corrAns) {
+            alert("Correct! +5 seconds");
+            scoreBonus(5);
+            nextQ();
+        } else {
+            alert("Wrong! -10 seconds");
+            scoreBonus(-10);
+            nextQ();
         };
-// function to advance quiz question
-function nextQ() {
-    current++;
-    if (current < 10) {
-        askQ = quiz[current].query;
-        askA = quiz[current].ansA;
-        askB = quiz[current].ansB;
-        askC = quiz[current].ansC;
-        askD = quiz[current].ansD;
-        corrAns = quiz[current].correct;
-
-        questionEl.innerHTML = askQ;
-        ansABtn.innerHTML = askA;
-        ansBBtn.innerHTML = askB;
-        ansCBtn.innerHTML = askC;
-        ansDBtn.innerHTML = askD; 
-    } else {
-        grade();
-    }
-};
-// function to set answer bonus/penalty
-function scoreBonus(ansBonus) {
-    secondsLeft += ansBonus
-};
-// function to end quiz
-function grade() {
-    clearInterval(timerInterval);
-    questionEl.classList.toggle("hide");
-    answer.classList.toggle("hide");
-}
-// function to toggle hidden class identifiers
-showSection();
-function showSection() {
-    b4Quiz.classList.toggle("hide");
-    quizEl.classList.toggle("hide");
-};
+        };
+    // function to advance quiz questions
+    function nextQ() {
+        current++;
+        if (current < 10) {
+            askQ = quiz[current].query;
+            askA = quiz[current].ansA;
+            askB = quiz[current].ansB;
+            askC = quiz[current].ansC;
+            askD = quiz[current].ansD;
+            corrAns = quiz[current].correct;
+            questionEl.innerHTML = askQ;
+            ansABtn.innerHTML = askA;
+            ansBBtn.innerHTML = askB;
+            ansCBtn.innerHTML = askC;
+            ansDBtn.innerHTML = askD; 
+        } else {
+            grade();
+        }
+    };
+    // function to set answer bonus/penalty
+    function scoreBonus(ansBonus) {
+        secondsLeft += ansBonus
+    };
+    // function to end quiz and prompt score save
+    function grade() {
+        clearInterval(timerInterval);
+        quizEl.classList.toggle("hide");
+        answer.classList.toggle("hide");  /* this may not be needed but seemed to be sometimes */
+        gradeArea.classList.toggle("hide");
+        timeEl.classList.toggle("hide");
+        gradeMessage.textContent = "Enter your initials to save your score of " + secondsLeft
+    };
+    // function to save grades to local storage
+    function saveLocalGrade(event) {
+        event.preventDefault();
+        let quizRound = {
+            pInput: pInitials.value.trim(),
+            pScore: secondsLeft,
+        }; console.log(quizRound);
+        let recordedGrades = JSON.parse(localStorage.getItem("recordedGrades")) || [];
+        recordedGrades.push(quizRound);
+        localStorage.setItem("recordedGrades",JSON.stringify(recordedGrades)); /* console.log(recordedGrades) */
+        showHallOfRecords();
+    };
+    // function to show hall of records high scores
+    function showHallOfRecords () {
+        gradeArea.classList.toggle("hide");
+        rankings.classList.toggle("hide");
+        // function to sort the saved scores in local storage and display in descending order
+        recordedGrades.sort(function(a,b) {
+            return b.pScore - a.pScore
+        });
+        for (rank = 0; rank<recordedGrades.length; rank++) {
+            let pRanksCreate = document.createElement("li");
+            pRanksCreate.textContent = rank.value();
+            sortedRanks.appendChild(pRanksCreate);
+            let pInitialsCreate = document.createElement("li");
+            pInitialsCreate.textContent = recordedGrades[rank].pInput;
+            sortedInitials.appendChild(pInitialsCreate);
+            let pScoresCreate = document.createElement("li");
+            pScoresCreate.textContent = recordedGrades[rank].pScore;
+            sortedScores.appendChild(pScoresCreate);
+        }
+    };
+    // function to toggle hidden class identifiers for hiding different sections
+    showSection();
+    function showSection() {
+        b4Quiz.classList.toggle("hide");
+        quizEl.classList.toggle("hide");
+    };
 // add event listeners
     // start button event
     startBtn.addEventListener("click", function () {
         setTime();
         showQuiz();
     });
+    // check for correct/wrong answers event
+    answer.addEventListener("click",checkAns);
+    // save score event
+    svGrdBtn.addEventListener("click",saveLocalGrade);
+    // show high scores event
